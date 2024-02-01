f011ced4ecd694089d1f59cdd723b185e185eeb3
RossCherry <rcherry1@harding.edu>
2024-02-01 13:41:43 -0600

    started updating console functions to Unity

--- Assets/Scripts/GamePiece.cs
+++ Assets/Scripts/GamePiece.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TreeEditor;
 using UnityEngine;
 
 public class GamePiece : MonoBehaviour
 {
+    public int row;
+    public int col;
+    public GameBoard board;
+    public GameObject piece;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +21,31 @@ public class GamePiece : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private bool CheckPickedPiece()
+    {
+        //Is piece an edge piece
+        if ((row == 0 || row == 4) || (col == 0 || col == 4))
+        {
+            //is piece blank
+            if (piece.CompareTag("-"))
+            {
+                return true;
+            }
+            //Is it the correct persons piece
+            else if (board.isPlayerOne && piece.CompareTag("X"))
+            {
+                return true;
+            }
+            //Is it the correct persons piece
+            else if (!board.isPlayerOne && piece.CompareTag("O"))
+            {
+                return true;
+            }
+
+        }
+        //It is not a playable piece
+        return false;
     }
 }
