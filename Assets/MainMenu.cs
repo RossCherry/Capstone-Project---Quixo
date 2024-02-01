44a9cd54d7f05af6108c702ede7e83c89c1d4e40
Swade7 <swade7@harding.edu>
2024-02-01 14:53:26 -0600

    Add start menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlaySinglePlayerEasy()
+    {
+        SceneManager.LoadSceneAsync("Game");
+    }
+
+    public void PlaySinglePlayerHard()
+    {
+        SceneManager.LoadSceneAsync("Game");
+    }
+
+    public void Help()
+    {
+        SceneManager.LoadSceneAsync("Help");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void PlayMultiplayerLocally()
+    {
+        SceneManager.LoadSceneAsync("Game");
+    }
+
+    public void PlayMultiplayerOverNetwork()
+    {
+        SceneManager.LoadSceneAsync("Game");
+    }
+}
+
