7ae2d4a059e884db49a172147044665d5cec5388
Swade7 <swade7@harding.edu>
2024-02-12 17:20:08 -0600

    Improve code layout

--- Assets/Scripts/GUI/MainMenu.cs
+++ Assets/Scripts/GUI/MainMenu.cs
@@ -5,34 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlaySinglePlayerEasy()
-    {
-        SceneManager.LoadSceneAsync("Game");
-    }
-
-    public void PlaySinglePlayerHard()
-    {
-        SceneManager.LoadSceneAsync("Game");
-    }
-
-    public void Help()
-    {
-        SceneManager.LoadSceneAsync("Help");
-    }
-
-    public void QuitGame()
-    {
-        Application.Quit();
-    }
-
-    public void PlayMultiplayerLocally()
-    {
-        SceneManager.LoadSceneAsync("Game");
-    }
-
-    public void PlayMultiplayerOverNetwork()
-    {
-        SceneManager.LoadSceneAsync("Game");
-    }
+    
 }
 
--- Assets/Scripts/GUI/Navigation.cs
+++ Assets/Scripts/GUI/Navigation.cs
@@ -1,17 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Navigation : MonoBehaviour
 {
     public void Help()
     {
-        // Navigate to the help screen
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Help");
+        SceneManager.LoadScene("Help");
     }
 
     public void MainMenu()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Main Menu");
+        SceneManager.LoadScene("Main Menu");
+    }
+
+
+    // TODO: Navigate to the correct scenes once they are created
+    public void PlaySinglePlayerEasy()
+    {
+        SceneManager.LoadSceneAsync("Game");
+    }
+
+    public void PlaySinglePlayerHard()
+    {
+        SceneManager.LoadSceneAsync("Game");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void PlayMultiplayerLocally()
+    {
+        SceneManager.LoadSceneAsync("Game");
+    }
+
+    public void PlayMultiplayerOverNetwork()
+    {
+        SceneManager.LoadSceneAsync("Game");
     }
 }
