dd365995d3239284bd7543db38eb84ed83100c08
RossCherry <rcherry1@harding.edu>
2024-02-13 13:31:47 -0600

    Revert "Merge pull request #6 from RossCherry/Options-Menu"
    This reverts commit a7bc190f039851bece17225a6aadd2e09d9775b0, reversing
    changes made to 95e3d1f572f5a6776f872a58a31bcbad851cfcfa.

--- Assets/Scripts/GUI/MainMenu.cs
+++ Assets/Scripts/GUI/MainMenu.cs
@@ -5,6 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    
+    public void PlaySinglePlayerEasy()
+    {
+        SceneManager.LoadSceneAsync("Game");
+    }
+
+    public void PlaySinglePlayerHard()
+    {
+        SceneManager.LoadSceneAsync("Game");
+    }
+
+    public void Help()
+    {
+        SceneManager.LoadSceneAsync("Help");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void PlayMultiplayerLocally()
+    {
+        SceneManager.LoadSceneAsync("Game");
+    }
+
+    public void PlayMultiplayerOverNetwork()
+    {
+        SceneManager.LoadSceneAsync("Game");
+    }
 }
 
